a4960406024ebbc2ae6d5580533ef926647696c5
nickngn <hungnd.iam@gmail.com>
2024-04-20 21:09:51 +0800

    Initiate

---
+++ src/main/java/com/nickngn/demotikaocr/DemoTikaOcrApplication.java
@@ -0,0 +1,13 @@
+package com.nickngn.demotikaocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTikaOcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTikaOcrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nickngn/demotikaocr/DemoTikaOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickngn.demotikaocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTikaOcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
